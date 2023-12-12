f4169b0dd12cbb85150b84ace82e613d8f99ff7f
immetras <eranusieq@gmail.com>
2023-12-12 23:33:54 +0100

    WIP, generating ships

--- index.js
+++ index.js
@@ -2,17 +2,39 @@
 function main() {
     const size = 10;
 
-    const botBoardHtml = document.getElementById("botBoard").children[0]
+
+    // generating plain bot table
+    const botBoard = document.getElementById("botBoard");
     // console.log(botBoardTable);
 
-    let botBoardArr = [];
+    const botBoardTable = document.createElement("table");
     for (let i = 0; i < size; i++) {
-        botBoardHtml.push([])
         const row = document.createElement("tr");
         for (let j = 0; j < size; j++) {
-            botBoardHtml[i].push("el")
             const col = document.createElement("td");
+            row.appendChild(col);
+        };
+        botBoardTable.appendChild(row);
+    };
+    botBoard.appendChild(botBoardTable);
+
+
+    // adding ships to bot table
+    const flags = 4;
+    let botShips = []
+    for (let i = 0; i < flags; i++) {
+        botShips.push([])
+        for (let j = 0; j < flags - i; j++) {
+            for (let k = 0; k < j + 1; k++) {
+                botShips[i].push(1);
+
+            }
         };
     };
+    console.table(botShips)
+
+    let botBoardArr = [];
+    botBoardArr.push([]);
+    botBoardArr[0].push(undefined);
     console.table(botBoardArr);
 };
