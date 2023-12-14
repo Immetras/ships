88de01e115ee2c2ea16cdfedf89e74ecefb55d46
immetras <eranusieq@gmail.com>
2023-12-14 14:29:23 +0100

    eciepecie

--- index.js
+++ index.js
@@ -47,52 +47,53 @@ function main() {
 
     // place ship on random posiotion
     function placeRandom(ship) {
-        // const orient = Math.floor(Math.random() * 2);
-        const orient = 1;
+        const len = botBoardArr.length;
+        const orient = Math.floor(Math.random() * 2);
+        // const orient = 0;
         let posX = 0;
         let posY = 0;
 
         switch (orient) {
             case 0:
-                posX = Math.floor(Math.random() * 10);
-                posY = Math.floor(Math.random() * (10 - ship));
+                posX = Math.floor(Math.random() * len);
+                posY = Math.floor(Math.random() * (len - (ship - 1)));
 
                 console.log(orient, posX, posY);
-                for (let i = -1; i < 2; i++) {
-                    posY != 0 ?
-                        botBoardArr[posX + i][posY - 1] = 0 : null;
-                };
+                // for (let i = -1; i < 2; i++) {
+                //     posY != 0 ?
+                //         botBoardArr[posX + i][posY - 1] = 0 : null;
+                // };
 
                 for (let i = 0; i < ship; i++) {
                     botBoardArr[posX][posY + i] = 1;
-                    botBoardArr[posX + 1][posY + i] = 0;
-                    botBoardArr[posX - 1][posY + i] = 0;
+                    // botBoardArr[posX + 1][posY + i] = 0;
+                    // botBoardArr[posX - 1][posY + i] = 0;
                 };
-                for (let i = -1; i < 2; i++) {
-                    posY != (botBoardArr.length - 1) ?
-                        botBoardArr[posX + i][posY + ship] = 0 : null;
-                };
+                // for (let i = -1; i < 2; i++) {
+                //     posY != (botBoardArr.length - 1) ?
+                //         botBoardArr[posX + i][posY + ship] = 0 : null;
+                // };
                 break;
 
             case 1:
-                posX = Math.floor(Math.random() * (10 - 3));
-                posY = Math.floor(Math.random() * 10);
+                posX = Math.floor(Math.random() * (len - (ship - 1)));
+                posY = Math.floor(Math.random() * len);
 
-                console.log(orient, posX, posY);
-                for (let i = -1; i < 2; i++) {
-                    posX != 0 ?
-                        botBoardArr[posX - 1][posY + i] = 0 : null;
-                };
+                // console.log(orient, posX, posY);
+                // for (let i = -1; i < 2; i++) {
+                //     posX != 0 ?
+                //         botBoardArr[posX - 1][posY + i] = 0 : null;
+                // };
 
                 for (let i = 0; i < ship; i++) {
                     botBoardArr[posX + i][posY] = 1;
-                    posY != (botBoardArr.length - 1) ? botBoardArr[posX + i][posY + 1] = 0 : null;
-                    posY != 0 ? botBoardArr[posX + i][posY - 1] = 0 : null;
+                    // posY != (botBoardArr.length - 1) ? botBoardArr[posX + i][posY + 1] = 0 : null;
+                    // posY != 0 ? botBoardArr[posX + i][posY - 1] = 0 : null;
                 };
-                for (let i = -1; i < 2; i++) {
-                    posX != (botBoardArr.length - 1) ?
-                        botBoardArr[posX + ship][posY + i] = 0 : null;
-                };
+                // for (let i = -1; i < 2; i++) {
+                //     posX != (botBoardArr.length - 1) ?
+                //         botBoardArr[posX + ship][posY + i] = 0 : null;
+                // };
 
                 break;
 
